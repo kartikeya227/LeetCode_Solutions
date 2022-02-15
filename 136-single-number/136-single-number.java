db009f159e2e9c2edf559a39a370bca5f6b8d229
kartikeya227 <kartikeya22798@gmail.com>
2022-02-15 11:16:22 +0530

    Time: 1 ms (95.92%), Space: 41.9 MB (71.93%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -2,6 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int sum = 0;
         for(int num : nums){
+            // Performing BIT manupulation read notes for extra info.
             sum ^= num;
         }
         return sum;
