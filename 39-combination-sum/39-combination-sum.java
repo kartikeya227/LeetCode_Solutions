8fedf0d3b27cfa8da62fee7085c710aeb3c57849
kartikeya227 <kartikeya22798@gmail.com>
2022-02-17 11:44:06 +0530

    Time: 18 ms (10.84%), Space: 44.9 MB (21.79%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,28 +1,39 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // Using Set so that we only store unique solutions
         Set<List<Integer>> combi = new HashSet<>();
         
+        // Calling the backtracking function for each index as a starting
         for(int i=0; i<candidates.length; i++){
             dfs(combi, new ArrayList<Integer>(), i, 0, candidates, target);
         }
+        
+        // Converting Set to List 
         return combi.stream().collect(Collectors.toList());
     }
     
+    // Backtracking function to add one element at a time and test if it will result in desired solution or not.
     boolean dfs(Set<List<Integer>> combi, List<Integer>tempList, int k, int sum, int[] candidates, int target){
+        // Base condition for success
         if(sum==target){
             combi.add(new ArrayList<>(tempList));
             return true;
-        } else if ( sum>target){
+        } 
+        // Base condition for case when set wont satisfy required condition
+        else if ( sum>target){
             return false;
         }
         for(int i=k; i<candidates.length; i++){
+            // No need to call dfs if we already know that input index is gonna result in a larger value than target.
             if((sum+candidates[i])>target)
                 continue;
             tempList.add(candidates[i]);
             sum += candidates[i];
             dfs(combi, tempList, i, sum, candidates, target);
+            // Backtracking -- start
             sum -= candidates[i];
             tempList.remove(tempList.size()-1);
+            // Backtracking --end
         }
         return false;
     }
