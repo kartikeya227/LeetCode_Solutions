3e96ef54e409bec6d798d91d433768a665ff14f4
kartikeya227 <kartikeya22798@gmail.com>
2022-03-01 12:50:08 +0530

    Time: 8 ms (21.25%), Space: 48.7 MB (10.44%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] countBits(int n) {
+        int i=0, j=0;
+        double two = Math.log(2);
+        int [] ans = new int[n+1];
+        ans[0] = 0;
+        for(i=1;i<=n;i++){
+            if(isLogBaseTwo(i, two)){
+                ans[i]=1;
+                j=i;
+            } else{
+                ans[i]=ans[i-j]+ans[j];
+            }
+        }
+        return ans;
+    }
+    
+    boolean isLogBaseTwo(int n, double two){
+        double f = Math.log(n)/two;
+        if(n==8192){
+            System.out.println(f);
+            System.out.println(Math.log(n)/Math.log(2));
+        }
+        if((f - (int)f) == 0.0)
+            return true;
+        return false;
+    }
+}
