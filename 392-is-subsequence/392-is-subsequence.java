64ebeb93e7817dfaaff6380c45a153e1c447545c
kartikeya227 <kartikeya22798@gmail.com>
2022-03-02 11:28:18 +0530

    Time: 2 ms (45.06%), Space: 42 MB (33.15%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         int i = s.length()-1, j = t.length()-1;
-        while(i>-1 && j>-1 && j>=i){
+        while(i>-1 && j>=i){
             if(s.charAt(i)==t.charAt(j))
                 i--;
             j--;
